0a0c3a91b3b244264b901240f190f9edeecaf82c
Aung Khant Kyaw <93343553+AungKyaw321@users.noreply.github.com>
2022-11-10 21:13:23 -0800

    UI Implemented (#58)
    * UI Implemented
    Co-authored-by: Nathan Ueda <nathanueda@users.noreply.github.com>
    Co-authored-by: Xuechen(Daniel) Kan <Daniel-Xkan@users.noreply.github.com>
    Co-authored-by: scottieboyzhang <scottieboyzhang@users.noreply.github.com>
    Co-authored-by: Jonathan Yin <joyin@ucsd.edu>
    
    * Fix tests and format code
    
    * Fix html test
    
    * Use external js for modal
    
    * Format input
    
    * Unit tests for openForm and closeForm
    
    Co-authored-by: Jett Bui <jettcbui@gmail.com>
    Co-authored-by: Samir Rashid <samirsgotmail@gmail.com>

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -2,7 +2,19 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("Window loaded");
+
     return 0;
 }
 
-module.exports = { init };
+function openForm() {
+    document.getElementById("myEntry").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myEntry").style.display = "none";
+}
+
+//document.getElementById("myEntry").onclick = openForm();
+//document.getElementById("myEntry").onclick = openForm()
+
+module.exports = { init, openForm, closeForm };
