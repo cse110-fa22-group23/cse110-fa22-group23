9260af7f234f2fc3ce4ef805d597576777803d9b
Samir Rashid <samirsgotmail@gmail.com>
2022-11-10 20:59:18 -0800

    Fix tests and format code

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -2,17 +2,16 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("Window loaded");
-   // return 0;
-   function openForm(){
-    document.getElementById("myEntry").style.display = "block";
-   }
-    function closeForm(){
+    function openForm() {
+        document.getElementById("myEntry").style.display = "block";
+    }
+    function closeForm() {
         document.getElementById("myEntry").style.display = "none";
     }
+    return 0;
 }
-
 
 //document.getElementById("myEntry").onclick = openForm();
 //document.getElementById("myEntry").onclick = openForm()
 
-//module.exports = { init };
+module.exports = { init };
