858c1ea84ddeecf751c26614411da86ba4af4a87
AungKyaw321 <93343553+AungKyaw321@users.noreply.github.com>
2022-11-10 20:02:31 -0800

    UI Implemented Co-authored-by: Nathan Ueda <nathanueda@users.noreply.github.com> Co-authored-by: Xuechen(Daniel) Kan <Daniel-Xkan@users.noreply.github.com> Co-authored-by: scottieboyzhang <scottieboyzhang@users.noreply.github.com> Co-authored-by: Jonathan Yin <joyin@ucsd.edu>

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -3,7 +3,17 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("Window loaded");
-    return 0;
+   // return 0;
+   function openForm(){
+    document.getElementById("myEntry").style.display = "block";
+   }
+    function closeForm(){
+        document.getElementById("myEntry").style.display = "none";
+    }
 }
 
-module.exports = { init };
+
+//document.getElementById("myEntry").onclick = openForm();
+//document.getElementById("myEntry").onclick = openForm()
+
+//module.exports = { init };
