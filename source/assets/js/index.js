62d56d699bef47757bf241e3832966d7488893a0
AungKyaw321 <93343553+AungKyaw321@users.noreply.github.com>
2022-11-12 15:39:30 -0800

    Finished making the buttons work, I want to see it on git pages

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -13,9 +13,25 @@ function openForm() {
 function closeForm() {
     document.getElementById("myEntry").style.display = "none";
 }
-
+function AddRow() {
+    var table = document.getElementById("spreadsheet");
+    var row = table.insertRow(1);
+    var company1 = row.insertCell(0);
+    var position1 = row.insertCell(1);
+    var location1 = row.insertCell(2);
+    var industry1 = row.insertCell(3);
+    var status1 = row.insertCell(4);
+    var ranking1 = row.insertCell(5);
+    //  var deadline1 = row.insertCell(6);
+    company1.innerHTML = document.getElementById("company").value;
+    position1.innerHTML = document.getElementById("position").value;
+    location1.innerHTML = document.getElementById("location").value;
+    industry1.innerHTML = document.getElementById("industry").value;
+    status1.innerHTML = document.getElementById("status").value;
+    ranking1.innerHTML = document.getElementById("ranking").value;
+}
 //document.getElementById("myEntry").onclick = openForm();
 //document.getElementById("myEntry").onclick = openForm()
 
 // To be used in tests
-module.exports = { init, openForm, closeForm };
+module.exports = { init, openForm, closeForm, AddRow };
