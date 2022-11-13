af76149d0002677415fd6bb9a33d36f6a7b46164
Jett Bui <jettcbui@gmail.com>
2022-11-12 20:52:58 -0800

    Add jsdocs for current functions

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -1,21 +1,28 @@
 window.addEventListener("DOMContentLoaded", init);
 
+/**
+ * Callback function to run when DOM is loaded.
+ * @return 0 (int)
+ */
 function init() {
     console.log("Window loaded");
 
     return 0;
 }
 
+/**
+ * Makes the form modal visible on the page.
+ */
 function openForm() {
     document.getElementById("myEntry").style.display = "block";
 }
 
+/**
+ * Makes the form modal no longer visible on the page.
+ */
 function closeForm() {
     document.getElementById("myEntry").style.display = "none";
 }
-
-//document.getElementById("myEntry").onclick = openForm();
-//document.getElementById("myEntry").onclick = openForm()
 
 // To be used in tests
 module.exports = { init, openForm, closeForm };
