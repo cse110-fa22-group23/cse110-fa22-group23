82224cabb54dff28a45884537e8d4ae385a002c6
AungKyaw321 <93343553+AungKyaw321@users.noreply.github.com>
2022-11-13 16:23:09 -0800

    UI input fixed problem with displaying data and implemented local storage

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -1,5 +1,5 @@
 //window.addEventListener("DOMContentLoaded", init);
-
+var data = [];
 /**
  * Callback function to run when DOM is loaded.
  * @return 0 (int)
@@ -28,6 +28,7 @@ function closeForm() {
  * Appends form data (from the modal) to a corresponding entry in the table.
  */
 function AddRow() {
+    event.preventDefault();
     var table = document.getElementById("spreadsheet");
     var row = table.insertRow(1);
     var company1 = row.insertCell(0);
@@ -44,11 +45,62 @@ function AddRow() {
     status1.innerHTML = document.getElementById("status").value;
     ranking1.innerHTML = document.getElementById("ranking").value;
     deadline1.innerHTML = document.getElementById("start").value;
-    closeForm();
+    // closeForm();
+    save_data();
 }
 
+/**
+ * Saves form data (from the modal) to local storage.
+ */
+
+function save_data() {
+    data.push({
+        company1: document.getElementById("company").value,
+        position1: document.getElementById("position").value,
+        location1: document.getElementById("location").value,
+        industry1: document.getElementById("industry").value,
+        status1: document.getElementById("status").value,
+        ranking1: document.getElementById("ranking").value,
+        deadline1: document.getElementById("start").value,
+    });
+    window.localStorage.setItem("SpreadSheet", JSON.stringify(data));
+    //  show_data(data);
+}
+/**
+function show_data(data){
+    var arr = data;
+    get_data(arr);
+    var table = document.getElementById("spreadsheet");
+    for(i = 0; i <= arr.length; i++){
+
+        var row = table.insertRow(1);
+        var company1 = row.insertCell(0);
+        var position1 = row.insertCell(1);
+        var location1 = row.insertCell(2);
+        var industry1 = row.insertCell(3);
+        var status1 = row.insertCell(4);
+        var ranking1 = row.insertCell(5);
+        var deadline1 = row.insertCell(6);
+        company1.innerHTML = arr[i].company1;
+        position1.innerHTML = arr[i].position1;
+        location1.innerHTML = arr[i].location1;
+        industry1.innerHTML = arr[i].industry1;
+        status1.innerHTML = arr[i].status1;
+        ranking1.innerHTML = arr[i].ranking1;
+        deadline1.innerHTML = arr[i].deadline1;
+    }
+
+}
+
+function get_data(data){
+    var str = localStorage.getItem("SpreadSheet");
+    if(str != null){
+        data = JSON.parse(str);
+    }
+}
+*/
 //document.getElementById("myEntry").onclick = openForm();
 //document.getElementById("myEntry").onclick = openForm()
 
 // To be used in tests
-module.exports = { init, openForm, closeForm, AddRow };
+module.exports = { init, openForm, closeForm, AddRow, save_data };
