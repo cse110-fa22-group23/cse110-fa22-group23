e720e4d5839294199e1b3dca097566eb4e3ab7a0
Xuechen(Daniel) Kan <107441641+Daniel-Xkan@users.noreply.github.com>
2022-11-22 01:00:48 -0800

    91 implement sort by column (#97)
    * Fix edit modal style, edit ordering, redundant code
    
    * Fix jsdoc merge conflicts
    
    * Added meeting notes.
    
    * Clean up null checks
    
    * finish sort and sort tag status change
    
    * fix ascending/descending icon
    
    * comment back buggy code
    
    * put the buggy star svg back so someone can fix it
    
    * comment out buggy module.exports
    
    * putting back the module exports again lol
    
    * fix lint problem
    
    * run prettier
    
    Co-authored-by: Samir Rashid <samirsgotmail@gmail.com>
    Co-authored-by: Nathan Ueda <nateueda@gmail.com>

--- source/assets/js/index.js
+++ source/assets/js/index.js
@@ -208,6 +208,78 @@ function save_localstorage() {
     window.localStorage.setItem("counter", JSON.stringify(count));
 }
 
+/////////////////////////SORT TABLE////////////////////////////////////////////////////////
+
+let cPrev = -1; // global var saves the previous c, used to
+// determine if the same column is clicked again
+
+function sortBy(c) {
+    let rows = document.getElementById("spreadsheet").rows.length - 1; // num of rows
+    // console.log(rows)
+    let columns = document.getElementById("spreadsheet").rows[0].cells.length; // num of columns
+    // console.log(columns)
+    let arrTable = [...Array(rows)].map(() => Array(columns)); // create an empty 2d array
+    // console.log(arrTable)
+
+    for (let ro = 0; ro < rows; ro++) {
+        // cycle through rows
+        for (let co = 0; co < columns; co++) {
+            // cycle through columns
+            // assign the value in each row-column to a 2d array by row-column
+            arrTable[ro][co] =
+                document.getElementById("spreadsheet").rows[ro].cells[
+                    co
+                ].innerHTML;
+        }
+    }
+
+    let th = arrTable.shift(); // remove the header row from the array, and save it
+
+    if (c !== cPrev) {
+        // different column is clicked, so sort by the new column
+        arrTable.sort((a, b) => {
+            if (a[c] === b[c]) {
+                return 0;
+            } else {
+                return a[c] < b[c] ? -1 : 1;
+            }
+        });
+    } else {
+        // if the same column is clicked then reverse the array
+        arrTable.reverse();
+    }
+
+    cPrev = c; // save in previous c
+
+    arrTable.unshift(th); // put the header back in to the array
+
+    // cycle through rows-columns placing values from the array back into the html table
+    for (let ro = 0; ro < rows; ro++) {
+        for (let co = 0; co < columns; co++) {
+            if (ro === 0) {
+                if (co == c) {
+                    let chosen =
+                        document.getElementById("spreadsheet").rows[ro].cells[
+                            co
+                        ];
+                    if (chosen.className == "sortable asc") {
+                        chosen.className = "sortable dsc";
+                    } else {
+                        chosen.className = "sortable asc";
+                    }
+                }
+            }
+            document.getElementById("spreadsheet").rows[ro].cells[
+                co
+            ].innerHTML = arrTable[ro][co];
+        }
+    }
+}
+let a = 0;
+if (a === 1) {
+    sortBy(0); // eliminate lint error
+}
+//////////////////////////END OF SORT TABLE//////////////////////////////////////////
 // To be used in tests
 module.exports = {
     init,
