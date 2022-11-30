e7d24d8991eba723b8162ad39bbaf30570f6324f
JonathanYin <joyin@ucsd.edu>
2022-11-29 18:42:25 -0800

    Init new sort method

--- source/assets/js/sort.js
+++ source/assets/js/sort.js
@@ -3,89 +3,63 @@
  * @param c column index to sort by
  */
 function sortBy(c) {
-    let rows = document.getElementById("spreadsheet").rows.length - 1; // num of rows
-    let columns = document.getElementById("spreadsheet").rows[0].cells.length; // num of columns
-    let arrTable = [...Array(rows)].map(() => Array(columns)); // create an empty 2d array
+    // let rows = document.getElementById("spreadsheet").rows.length - 1; // num of rows
+    // let columns = document.getElementById("spreadsheet").rows[0].cells.length; // num of columns
 
-    for (let ro = 0; ro < rows; ro++) {
-        // cycle through rows
-        for (let co = 0; co < columns; co++) {
-            // cycle through columns
-            // assign the value in each row-column to a 2d array by row-column
-            arrTable[ro][co] =
-                document.getElementById("spreadsheet").rows[ro].cells[
-                    co
-                ].innerHTML;
-        }
-    }
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("spreadsheet");
+    switching = true;
+    /* Make a loop that will continue until
+    no switching has been done: */
+    var dir = "asc";
+    var switchcount = 0;
 
-    let th = arrTable.shift(); // remove the header row from the array, and save it
-
-    // if the same column is clicked then reverse the array
-
-    arrTable.sort((a, b) => {
-        if (a[c] === b[c]) {
-            return 0;
-        } else {
-            return a[c] < b[c] ? -1 : 1;
-        }
-    });
-    if (
-        document.getElementById("spreadsheet").rows[0].cells[c].className ==
-            "sortable asc" ||
-        document.getElementById("spreadsheet").rows[0].cells[c].className ==
-            "sortable asc selected"
-    ) {
-        arrTable.reverse();
-    }
-
-    arrTable.unshift(th); // put the header back in to the array
-
-    // cycle through rows-columns placing values from the array back into the html table
-    for (let ro = 0; ro < rows; ro++) {
-        for (let co = 0; co < columns; co++) {
-            if (ro === 0 && co === c) {
-                let chosen =
-                    document.getElementById("spreadsheet").rows[ro].cells[co];
-                if (chosen.className == "sortable asc selected") {
-                    chosen.className = "sortable dsc selected";
-                } else if (chosen.className == "sortable dsc selected") {
-                    chosen.className = "sortable asc selected";
-                } else if (chosen.className == "sortable asc") {
-                    chosen.className = "sortable dsc";
-                } else {
-                    chosen.className = "sortable asc";
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+            first, which contains table headers): */
+        for (i = 1; i < rows.length - 1; i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+                one from current row and one from the next: */
+            x = rows[i].cells[c];
+            y = rows[i + 1].cells[c];
+            console.log(x);
+            console.log(y);
+            // Check if the two rows should switch place:
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
                 }
             }
-            document.getElementById("spreadsheet").rows[ro].cells[
-                co
-            ].innerHTML = arrTable[ro][co];
-            // TODO: restore the status class here
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+                and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
         }
     }
-    // fix status color bubble
-    for (let ro = 1; ro < rows; ro++) {
-        let chosen = document.getElementById("spreadsheet").rows[ro].cells[4];
-        if (chosen.innerHTML == "Applied") {
-            chosen.className = "applied";
-        } else if (chosen.innerHTML == "In Progress") {
-            chosen.className = "in_progress";
-        } else if (chosen.innerHTML == "Not Started") {
-            chosen.className = "not_started";
-        }
-    }
-    // fix status color bubble
-    for (let ro = 1; ro < rows; ro++) {
-        let chosen = document.getElementById("spreadsheet").rows[ro].cells[4];
-        if (chosen.innerHTML == "Applied") {
-            chosen.className = "applied";
-        } else if (chosen.innerHTML == "In Progress") {
-            chosen.className = "in_progress";
-        } else if (chosen.innerHTML == "Not Started") {
-            chosen.className = "not_started";
-        }
-    }
-
     const selectedHeaders = document.querySelectorAll(".selected");
     // remove selected status from other cols
     selectedHeaders.forEach((header) => {
