cfb2f9e94fa3238c494a865d0085de261f635f6b
JonathanYin <joyin@ucsd.edu>
2022-11-30 16:02:52 -0800

    Fixed header arrow not displaying correctly

--- source/assets/js/sort.js
+++ source/assets/js/sort.js
@@ -3,9 +3,6 @@
  * @param c column index to sort by
  */
 function sortBy(c) {
-    // let rows = document.getElementById("spreadsheet").rows.length - 1; // num of rows
-    // let columns = document.getElementById("spreadsheet").rows[0].cells.length; // num of columns
-
     var table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("spreadsheet");
     switching = true;
@@ -27,8 +24,7 @@ function sortBy(c) {
                 one from current row and one from the next: */
             x = rows[i].cells[c];
             y = rows[i + 1].cells[c];
-            // console.log(x);
-            // console.log(y);
+
             // Check if the two rows should switch place:
             if (dir == "asc") {
                 if (
@@ -54,27 +50,38 @@ function sortBy(c) {
         }
         if (shouldSwitch) {
             /* If a switch has been marked, make the switch
-                and mark that a switch has been done: */
+                        and mark that a switch has been done: */
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
+
             // Each time a switch is done, increase this count by 1:
             switchcount++;
         } else {
             /* If no switching has been done AND the direction is "asc",
-                set the direction to "desc" and run the while loop again. */
+                        set the direction to "desc" and run the while loop again. */
             if (switchcount == 0 && dir == "asc") {
                 dir = "desc";
                 switching = true;
             }
         }
     }
+    // remove selected, asc, and desc statuses from other cols
     const selectedHeaders = document.querySelectorAll(".selected");
-    // remove selected status from other cols
+
     selectedHeaders.forEach((header) => {
         header.classList.remove("selected");
+        header.classList.remove("asc");
+        header.classList.remove("desc");
     });
-    console.log(table);
+
+    // add selected status to current col
     table.rows[0].cells[c].classList.add("selected");
+
+    if (dir == "asc") {
+        table.rows[0].cells[c].classList.add("asc");
+    } else {
+        table.rows[0].cells[c].classList.add("desc");
+    }
 }
 
 export { sortBy };
