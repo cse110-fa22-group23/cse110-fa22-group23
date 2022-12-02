65f8c5cb9c5d29a3e146a13969f385770a680556
Jett Bui <jettcbui@gmail.com>
2022-12-02 13:45:50 -0800

    Refactor sort (2)

--- source/assets/js/sort.js
+++ source/assets/js/sort.js
@@ -3,14 +3,13 @@
  * @param c column index to sort by
  */
 function sortBy(c) {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("spreadsheet");
-    switching = true;
-
+    var table = document.getElementById("spreadsheet");
+    var switching = true;
     var dir = "asc";
     var switchcount = 0;
 
     // loop until no more switching has been done
+    var rows, i, x, y, shouldSwitch;
     while (switching) {
         // default to no switching
         switching = false;
@@ -59,7 +58,6 @@ function sortBy(c) {
     }
     // remove "selected", "asc", and "desc" statuses from other cols
     const selectedHeaders = document.querySelectorAll(".selected");
-
     selectedHeaders.forEach((header) => {
         header.classList.remove("selected");
         header.classList.remove("asc");
@@ -69,12 +67,8 @@ function sortBy(c) {
     // add selected status to current col
     table.rows[0].cells[c].classList.add("selected");
 
-    // change table header arrow based on sort
-    if (dir == "asc") {
-        table.rows[0].cells[c].classList.add("asc");
-    } else {
-        table.rows[0].cells[c].classList.add("desc");
-    }
+    // change table header arrow based on sort (dir is asc or desc)
+    table.rows[0].cells[c].classList.add(dir);
 }
 
 export { sortBy };
